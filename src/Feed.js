e2b3d0c88871c1ebc4b3d9dbcd46cd8984b5b608
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2020-09-20 15:55:08 +0630

    added message sender and post components

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Feed.css';
+import MessageSender from './MessageSender';
+import Post from './Post';
 import StoryReel from './StoryReel';
 
 function Feed() {
@@ -8,6 +10,23 @@ function Feed() {
             {/* Story Section */}
             <StoryReel />
             {/* Message Sender */}
+            <MessageSender />
+
+            <Post
+                profilePic="https://cdn.pixabay.com/photo/2020/09/14/17/17/beach-5571533__340.jpg"
+                message="I am on Cloud9"
+                timestamp="this is a time stamp"
+                username="chan myae oo"
+                image="https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988__340.jpg"
+            />
+
+            <Post
+                profilePic="https://cdn.pixabay.com/photo/2020/09/14/17/17/beach-5571533__340.jpg"
+                message="I am on Cloud9"
+                timestamp="this is a time stamp"
+                username="chan myae oo"
+                image="https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988__340.jpg"
+            />
         </div>
     )
 }
