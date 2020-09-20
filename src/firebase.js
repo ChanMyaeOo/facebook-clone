e26cc679b0db32658cae9f80d4961d50b4726479
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2020-09-20 11:26:06 +0630

    finished header section

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAf3TyZedFTOSuVzpMfbX4SzWfrv_vm1EI",
+    authDomain: "facebook-clone-d3008.firebaseapp.com",
+    databaseURL: "https://facebook-clone-d3008.firebaseio.com",
+    projectId: "facebook-clone-d3008",
+    storageBucket: "facebook-clone-d3008.appspot.com",
+    messagingSenderId: "685422234842",
+    appId: "1:685422234842:web:4a9fadb12d3b345288a9e3"
+  };
